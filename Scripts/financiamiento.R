3e7f9d272cf5af2acde4d5d84a1a84fb6f2c7afa
smendeza1 <smendez@axoncg.com>
2016-12-01 17:28:43 -0600

    Creación de funcion de calculo de amortizacion
    Esta funcion debe de servir para realizar los calculos de cada inversion que se debe de realizar

--- Scripts/financiamiento.R
+++ Scripts/financiamiento.R
@@ -17,8 +17,39 @@ i.super <- Inversiones %>%
   filter(Inversion.Superestructura!=0) %>%
   select(-Inversion.Infraestructura)
 
-n <- 30 #Años de financiamiento
-r <- 0.05 #Tasa a la que se trabaja
+n <- 30 # Años de financiamiento
+r <- 0.05 # Tasa a la que se trabaja
+pg <- 3 # Años de gracia
 
+inversion <- i.infr[1,]
+inversion[,3] <- 29414
 
+amortizacion <- function(n=30,r=0.05,pg=3,inversion){
 
+# al momento de crear el escenario sumo n+pg por el periodo de gracia de cada pago
+escenario <- data_frame(Year=numeric(n+pg), 
+                        Saldo=numeric(n+pg),
+                        Intereses =numeric(n+pg),
+                        Amortizacion=numeric(n+pg))
+escenario$Year <- seq_len(n+pg) + as.numeric(inversion$Year) # Llenado de los años correspondientes al escenario
+escenario$Amortizacion <- inversion$Inversion.Infraestructura/n # Calculo de los pagos de amortización anuales
+
+if(pg==0){
+  
+  escenario <- escenario %>% 
+    mutate(Saldo = inversion$Inversion.Infraestructura-cumsum(escenario$Amortizacion),
+           Intereses = (Saldo+Amortizacion)*r)
+  
+}else {
+  escenario$Amortizacion[1:pg] <- 0 # Seteo de periodo de gracia
+  escenario <- escenario %>% 
+    mutate(Saldo = inversion$Inversion.Infraestructura-cumsum(escenario$Amortizacion),
+           Intereses = (Saldo+Amortizacion)*r)
+  escenario$Intereses[1:pg] <- 0 # Seteo de periodo de gracia
+}
+
+escenario  <- escenario %>% mutate(Pago=Intereses+Amortizacion)
+return(escenario)
+}
+
+amortizacion(inversion = i.infr[3,])
