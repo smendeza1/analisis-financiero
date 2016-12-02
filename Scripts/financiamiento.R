d06919f858377da27c211772037e25cfb3f73222
smendeza1 <smendez@axoncg.com>
2016-12-01 18:00:18 -0600

    Agregar periodo de gracia con prueba logica
    Se realiza un test que calcula automaticamente cuanto tiempo debe de ser el periodo de gracia para cada inversion. Se arregla un bug que no permitia ingresar inversiones de superestructura

--- Scripts/financiamiento.R
+++ Scripts/financiamiento.R
@@ -3,11 +3,14 @@ source("Scripts/Costos e Inversiones.R")
 source("Scripts/fin101.R")
 
 
+# Creacion de funcion de amortización -------------------------------------
+
 # Tengo que considerar que pueden existir distintas modalidades de realizar el financiamiento, por ejemplo
     # Podrian solicitarse creditos por tipo de inversion (Infraestructura/Superestrucutra)
     # Podría interesarnos desglosar las inversiones por tipo de sistema principal/secundario (Ej: Intermodal, compuesto por puertos y ferrocarril)
 # En base a lo anterior se van a realizar los escenarios de inversion para cada desembolso que podemos visualizar con sus coordenadas. 
 # En el caso de que necesitemos mas visibilidad, podremos visualizar esas composiciones
+
 
 i.infr <- Inversiones %>% #Realizo la separacion para poder dividir el problema en pequeños problemas y luego juntarlo
   filter(Inversion.Infraestructura!=0) %>%
@@ -17,14 +20,34 @@ i.super <- Inversiones %>%
   filter(Inversion.Superestructura!=0) %>%
   select(-Inversion.Infraestructura)
 
-n <- 30 # Años de financiamiento
-r <- 0.05 # Tasa a la que se trabaja
-pg <- 3 # Años de gracia
+# n <- 30 # Años de financiamiento
+# r <- 0.05 # Tasa a la que se trabaja
+# pg <- 3 # Años de gracia para poder construir los sistemas
 
-inversion <- i.infr[1,]
-inversion[,3] <- 29414
+amortizacion <- function(n=30,r=0.05,inversion){
 
-amortizacion <- function(n=30,r=0.05,pg=3,inversion){
+  names(inversion)[3] <- "Inversion"
+
+# Prueba logica para determinar periodo de gracia -------------------------
+
+  year.inversion <- as.numeric(inversion$Year) # año en que se realiza la inversion
+  source("Scripts/ingresos.R")
+  
+  primer.ingreso <- ingresos %>% filter(Ingresos.Brutos!=0) %>%
+    group_by(Year)%>%
+    summarise(Ingresos.Brutos=sum(Ingresos.Brutos))
+  
+  primer.ingreso <- min(as.numeric(primer.ingreso$Year)) # año en el que existe el primer ingreso
+  
+  if(year.inversion< primer.ingreso  ){
+    pg = primer.ingreso - year.inversion -1 
+  }else{
+    pg = 0 
+  }
+  
+  
+# creacion de escenario inversión -----------------------------------------
+
 
 # al momento de crear el escenario sumo n+pg por el periodo de gracia de cada pago
 escenario <- data_frame(Year=numeric(n+pg), 
@@ -32,18 +55,18 @@ escenario <- data_frame(Year=numeric(n+pg),
                         Intereses =numeric(n+pg),
                         Amortizacion=numeric(n+pg))
 escenario$Year <- seq_len(n+pg) + as.numeric(inversion$Year) # Llenado de los años correspondientes al escenario
-escenario$Amortizacion <- inversion$Inversion.Infraestructura/n # Calculo de los pagos de amortización anuales
+escenario$Amortizacion <- inversion$Inversion/n # Calculo de los pagos de amortización anuales
 
 if(pg==0){
   
   escenario <- escenario %>% 
-    mutate(Saldo = inversion$Inversion.Infraestructura-cumsum(escenario$Amortizacion),
+    mutate(Saldo = inversion$Inversion-cumsum(escenario$Amortizacion),
            Intereses = (Saldo+Amortizacion)*r)
   
 }else {
   escenario$Amortizacion[1:pg] <- 0 # Seteo de periodo de gracia
   escenario <- escenario %>% 
-    mutate(Saldo = inversion$Inversion.Infraestructura-cumsum(escenario$Amortizacion),
+    mutate(Saldo = inversion$Inversion-cumsum(escenario$Amortizacion),
            Intereses = (Saldo+Amortizacion)*r)
   escenario$Intereses[1:pg] <- 0 # Seteo de periodo de gracia
 }
@@ -52,4 +75,8 @@ escenario  <- escenario %>% mutate(Pago=Intereses+Amortizacion)
 return(escenario)
 }
 
-amortizacion(inversion = i.infr[3,])
+amortizacion(inversion = i.super[8,])
+
+
+
+
