237d5b907c656b86b323cbbd7f4f206f0292b692
smendeza1 <smendez@axoncg.com>
2016-12-16 17:37:24 -0600

    Agregar opciones de inputs
    Se reordena y se agrega tipo de regimen fiscal, horizonte vpn, y slider para seleccionar valor de isr sobre utilidades

--- app/ui.R
+++ app/ui.R
@@ -7,15 +7,20 @@ ui <- navbarPage(title="Analisis Financiero",
   tabPanel("VPN",
   sidebarLayout(
     sidebarPanel(
-      sliderInput("años.financiamiento",label = "Años financiamiento",value = 30,max=50,min=5),
+      sliderInput("años.financiamiento",label = "Años financiamiento",value = 30,max=30,min=5),
+      sliderInput("horizonte","Año final evaluación VPN",value = 2080,max=2112,min=2050,step=2),
       sliderInput("tasa.financiamiento","Tasa financiamiento",value=0.05,max=0.1,min=0.01,step = 0.005),
       sliderInput("pct.financiado","Porcentaje Inversion Financiada",min = 0,max=1,value=0.6),
-      sliderInput("isr","Impuesto sobre la renta: Sobre ingresos",min=0.01,max=0.1,value=0.07,step=0.005),
       sliderInput("tasa.descuento","Tasa de Descuento",value=0.08,max = 0.2,min=0.05,step=0.005),
       helpText("Esta es la tasa a la que se evalúa el proyecto para el promotor y el operador"),
       radioButtons("tipo.demanda","Tipo demanda",
                    choices = list("Demanda Minima"="min","Demanda Maxima"="max"),
-                   selected = "min")
+                   selected = "min"),
+      radioButtons("regimen.fiscal", "Regimen Fiscal",
+                   choices = list("Sobre Ingresos"=FALSE,"Sobre Utilidades"=TRUE),
+                   selected = FALSE),
+      sliderInput("isr","Impuesto sobre la renta: Sobre ingresos",min=0.01,max=0.1,value=0.07,step=0.005),
+      sliderInput("isr2","Impuesto sobre la renta: Sobre utilidades",min=0.25,max=0.4,value = 0.25,step=0.05)
     ),
     mainPanel(tabsetPanel(
       tabPanel("Resultado analisis",textOutput("modelo")),
