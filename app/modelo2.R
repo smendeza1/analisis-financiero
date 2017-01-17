f9978e06d5c7a18c4428fa3e5fab094263fa7372
smendeza1 <smendez@axoncg.com>
2017-01-17 16:29:25 -0600

    Calcular FEN

--- app/modelo2.R
+++ app/modelo2.R
@@ -1,5 +1,4 @@
 # # Información utilizada por la función para poder funcionar
-source("app/famortizacion.R") # funciones para calculos financieros
 Demanda <- read_excel("app/Datos/Intermodal/Ingresos.xlsx", sheet = "Demanda")
 Tarifas <- read_excel("app/Datos/Intermodal/Ingresos.xlsx", sheet = "Tarifas")
 Costos <- read_excel("app/Datos/Intermodal/Costos e Inversiones.xlsx", sheet = "Costos")
@@ -132,7 +131,6 @@ Ingresos <- Ingresos %>%
          Sistema = as.factor(Sistema),
          Elemento = as.factor(Elemento))
 
-primer.año.ingreso <- min(Ingresos$Año[Ingresos$Ingresos.Brutos != 0])
 
 segmentar_ingreso <- function(df = Ingresos, type = "Sistema"){
   
@@ -141,16 +139,18 @@ segmentar_ingreso <- function(df = Ingresos, type = "Sistema"){
     split(Ingresos$Sistema) %>%
     map(group_by, Año) %>%
     map(select, -Elemento, -Sistema) %>%
-    map(summarise_all, sum),
+    map(summarise_all, sum) %>% 
+    map(mutate, ISR = Ingresos.Brutos*isr),
   Elemento = Ingresos %>%
     split(Ingresos$Elemento) %>%
     map(group_by, Año) %>%
     map(select, -Elemento, -Sistema) %>%
-    map(summarise_all, sum))
+    map(summarise_all, sum)%>% 
+    map(mutate, ISR = Ingresos.Brutos*isr))
   }
   
-ingresos_sistema11 <- segmentar_ingreso(type = "Sistema")
-ingresos_elemento11 <- segmentar_ingreso(type = "Elemento")
+ingresos.sistema11 <- segmentar_ingreso(type = "Sistema")
+ingresos.elemento11 <- segmentar_ingreso(type = "Elemento")
 
 # Inversiones y canones de infraestructura y concesión -------------------------------------------------------------
 
@@ -200,7 +200,6 @@ inversiones.sistema10 <- segmentar_inversion(Inversiones, type = "ST")
 inversiones.elemento11 <- segmentar_inversion(Inversiones, type = "EF")
 inversiones.elemento10 <- segmentar_inversion(Inversiones, type = "ET")
 
-inversiones.elemento11
 
 ## Funcion que sirve para calcular los anticipos y las anualidades de cada canon
 ## en donde canon puede referirse al valor de renta de infraestructura o concesión del corredor
@@ -282,7 +281,7 @@ Costos
 segmentar_costos <- function(df = Costos, type) {
   require(dplyr)
   require(purrr)
-  
+
   switch(type,
          SF = df %>% 
            mutate(Sistema = as.factor(Sistema)) %>%
@@ -298,7 +297,7 @@ segmentar_costos <- function(df = Costos, type) {
            map(gather, Año, Valor, -c(Sistema:Componente)) %>%
            map(mutate, Año = as.factor(Año)) %>%
            map(group_by, Año, Componente) %>%
-           map(summarise, Inversion = sum(Valor))%>% 
+           map(summarise, Costos = sum(Valor)) %>% 
            map(mutate, IVAxCobrar = Costos*0.12),
          EF = df %>% 
            mutate(Elemento = as.factor(Elemento)) %>%
@@ -306,7 +305,7 @@ segmentar_costos <- function(df = Costos, type) {
            map(gather, Año, Valor, -c(Sistema:Componente)) %>%
            map(mutate, Año = as.factor(Año)) %>%
            map(group_by, Año) %>%
-           map(summarise, Inversion = sum(Valor))%>% 
+           map(summarise, Costos = sum(Valor)) %>% 
            map(mutate, IVAxCobrar = Costos*0.12),
          ET = df %>% 
            mutate(Elemento = as.factor(Elemento)) %>%
@@ -314,17 +313,18 @@ segmentar_costos <- function(df = Costos, type) {
            map(gather, Año, Valor, -c(Sistema:Componente)) %>%
            map(mutate, Año = as.factor(Año)) %>%
            map(group_by, Año, Componente) %>%
-           map(summarise, Inversion = sum(Valor))%>% 
+           map(summarise, Costos = sum(Valor)) %>% 
            map(mutate, IVAxCobrar = Costos*0.12))
   
 }
 
 costos.sistema11 <- segmentar_costos(type = "SF")
-
-
+costos.sistema10 <- segmentar_costos(type = "ST")
+costos.elemento11 <- segmentar_costos(type = "EF")
+costos.elemento10 <- segmentar_costos(type = "ET")
 # IVA e ISR ---------------------------------------------------------------------
 
-Impuestos11 <- map2(ingresos_sistema11,inversiones.sistema11,left_join) %>%
+Impuestos11 <- map2(ingresos.sistema11,inversiones.sistema11,left_join) %>%
   map2(costos.sistema11, left_join, by = "Año") %>%
   map(mutate, IVAxCobrar = IVAxCobrar.x + IVAxCobrar.y) %>%
   map(select, -Inversion, -Royalty, -Ingresos.Brutos, -IVAxCobrar.x, -IVAxCobrar.y, -Costos ) %>% 
@@ -337,7 +337,163 @@ Impuestos11 <- map2(ingresos_sistema11,inversiones.sistema11,left_join) %>%
 
 # Financiamiento ----------------------------------------------------------
 
-inversiones.sistema11
+df <- inversiones.sistema11 %>%
+  map2(ingresos_sistema11,left_join)
+df <- df$Multimodal
+
+## aqui empezaria la funcion
+
+amortizacion <- function(df,pct.financiado = 1, n = 30, r = 0.07 ){
+require(tidyverse)
+    
+n.inversiones <- df %>% filter(Inversion != 0) %>% nrow()
+
+primer.ingreso <- filter(df, Ingresos.Brutos != 0)
+primer.ingreso <- primer.ingreso$Año %>%  as.character() %>% as.numeric() %>% min()
+
+df <- df %>% filter(Inversion != 0)
+inversion.input <- df[1,]
+inversion.input$Inversion <- inversion.input$Inversion*pct.financiado
+año.inversion <- inversion.input$Año %>% as.character() %>% as.numeric()
+
+### Definicion periodo de gracia
+if (año.inversion < primer.ingreso ) {
+  pg = primer.ingreso - año.inversion
+}else{
+  pg = 0 
+}
+
+# creacion de escenario inversión -----------------------------------------
+## al momento de crear el escenario sumo n+pg por el periodo de gracia de cada pago
+escenario <- data_frame(Año = numeric(n + pg), 
+                        Saldo = numeric(n + pg),
+                        Intereses = numeric(n + pg),
+                        Amortizacion = numeric(n + pg))
+escenario$Año <- seq_len(n + pg) + año.inversion - 1 # Llenado de los años correspondientes al escenario
+escenario$Amortizacion <- inversion.input$Inversion/n # Calculo de los pagos de amortización anuales
+ 
+
+if (pg == 0) {
+  
+  escenario <- escenario %>% 
+    mutate(Saldo = inversion.input$Inversion - cumsum(escenario$Amortizacion),
+           Intereses = (Saldo + Amortizacion)*r)
+  
+}else {
+  escenario$Amortizacion[1:pg] <- 0 # Set de periodo de gracia
+  escenario <- escenario %>% 
+    mutate(Saldo = inversion.input$Inversion - cumsum(escenario$Amortizacion),
+           Intereses = (Saldo + Amortizacion)*r)
+}
+
+
+if(exists("Componente",df)) {
+  escenario  <- escenario %>% mutate(Pago = Intereses + Amortizacion)
+  escenario$Componente <- inversion.input$Componente
+} else {
+  escenario  <- escenario %>% mutate(Pago = Intereses + Amortizacion)
+}
+
+for (i in 2:n.inversiones) {
+  inversion.input <- df[i,]
+  inversion.input$Inversion <- inversion.input$Inversion*pct.financiado
+  año.inversion <- inversion.input$Año %>% as.character() %>% as.numeric()
+  
+  ### Definicion periodo de gracia
+  if (año.inversion < primer.ingreso ) {
+    pg = primer.ingreso - año.inversion
+  }else{
+    pg = 0 
+  }
+  
+  # creacion de escenario inversión -----------------------------------------
+  ## al momento de crear el escenario sumo n+pg por el periodo de gracia de cada pago
+  tmp <- data_frame(Año = numeric(n + pg), 
+                          Saldo = numeric(n + pg),
+                          Intereses = numeric(n + pg),
+                          Amortizacion = numeric(n + pg))
+  tmp$Año <- seq_len(n + pg) + año.inversion - 1 # Llenado de los años correspondientes al escenario
+  tmp$Amortizacion <- inversion.input$Inversion/n # Calculo de los pagos de amortización anuales
+  
+  
+  if (pg == 0) {
+    
+    tmp <- tmp %>% 
+      mutate(Saldo = inversion.input$Inversion - cumsum(tmp$Amortizacion),
+             Intereses = (Saldo + Amortizacion)*r)
+    
+  }else {
+    tmp$Amortizacion[1:pg] <- 0 # Set de periodo de gracia
+    tmp <- tmp %>% 
+      mutate(Saldo = inversion.input$Inversion - cumsum(tmp$Amortizacion),
+             Intereses = (Saldo + Amortizacion)*r)
+  }
+  
+  if (exists("Componente",df)) {
+    tmp  <- tmp %>% mutate(Pago = Intereses + Amortizacion)
+    tmp$Componente <- inversion.input$Componente
+  } else {
+    tmp  <- tmp %>% mutate(Pago = Intereses + Amortizacion)
+  }
+  
+  escenario <- rbind(escenario, tmp)
+}
+
+if (exists("Componente",df)) {
+  escenario <- escenario %>% 
+    group_by(Año, Componente) %>% 
+    summarise_all(sum)
+  escenario 
+}else{
+  escenario <- escenario %>% 
+    group_by(Año) %>% 
+    summarise_all(sum)
+  escenario$Año <- as.factor(escenario$Año)
+  escenario
+}
+escenario$Año <- as.factor(escenario$Año)
+escenario
+}
+
+financiamiento.sistema11 <- inversiones.sistema11 %>% 
+  map2(ingresos.sistema11, left_join) %>% 
+  map(amortizacion,pct.financiado = pct.financiado, n = n, r = r)
 
 
 
+
+inversiones.sistema11 <- inversiones.sistema11 %>% 
+  map( function(df) df %>% mutate(Inversion = Inversion * (1 - pct.financiado)))
+
+df <- ingresos.sistema11 %>%
+  map2(costos.sistema11, full_join) %>%
+  map(select, -contains("IVA")) %>% 
+  map2(inversiones.sistema11, full_join) %>% 
+  map(select, -contains("IVA")) %>% 
+  map2(financiamiento.sistema11, full_join) %>%
+  map(select, -Saldo, -Intereses, -Amortizacion) %>% 
+  map2(Impuestos11, full_join) %>% 
+  map(select, -contains("IVAx")) %>% 
+  map(replace_na, replace = list(Ingresos.Brutos = 0,
+                                 Royalty = 0,
+                                 ISR = 0,
+                                 Costos = 0,
+                                 Inversion = 0,
+                                 Pago = 0,
+                                 IVA.Neto = 0)) 
+
+df <- df$Multimodal
+
+# calcular_fen <- function(df,r,)
+  
+df <-  df %>% 
+  mutate(IVA.Neto = if_else(IVA.Neto > 0,0,IVA.Neto),
+         FEN      =  Ingresos.Brutos
+         - ISR
+         - Costos
+         - Inversion
+         - Pago
+         - IVA.Neto)
+
+npv(r = r, cf = df$FEN)
+irr2(df$FEN)
