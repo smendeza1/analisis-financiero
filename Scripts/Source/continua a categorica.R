fbdb51801c1cecdbd39c6c5db261b8c72ad3f331
smendeza1 <smendez@axoncg.com>
2017-02-15 16:56:04 -0600

    Quitar carpetas que estorban

--- Scripts/Source/continua a categorica.R
+++
@@ -1,8 +0,0 @@
-
-convertir_continua_categorica <- function(x, n = 6, style = "quantile" ){
-  require(classInt)
-  require(Hmisc)
-  brks <- classIntervals(x, n = 6, style = style)
-  p <- cut2(x, cuts = p$brks)
-  p
-}
